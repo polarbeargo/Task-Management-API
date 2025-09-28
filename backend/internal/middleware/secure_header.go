35b4afa84d325cfa4ad5f384d79711d0ab14666c
Hsin-Wen Chang <bow1226@gmail.com>
2025-09-29 07:34:33 +0800

    feat: Implement Strict-Transport-Security header to middleware
    Added the 'Strict-Transport-Security' header to the SecureHeader middleware to enforce HTTPS and improve security for all responses.

--- backend/internal/middleware/secure_header.go
+++ backend/internal/middleware/secure_header.go
@@ -6,6 +6,7 @@ import (
 
 func SecureHeader() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		c.Writer.Header().Set("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
 		c.Writer.Header().Set("X-Content-Type-Options", "nosniff")
 		c.Writer.Header().Set("X-Frame-Options", "DENY")
 		c.Writer.Header().Set("X-XSS-Protection", "1; mode=block")
